38c7aebba7454507c61b78d332798e97ec6d786c
NoahELE <cxhmel@outlook.com>
2024-05-04 22:20:06 +1000

    update test connection

--- test_connection.py
+++ test_connection.py
@@ -5,15 +5,17 @@ from elasticsearch import Elasticsearch
 from k8s_utils import config
 
 try:
-    client = Elasticsearch(
-        f"{config("ES_HOST")}/{config("ES_DATABASE")}",
-        basic_auth=(config("ES_USERNAME"), config("ES_PASSWORD")),
-    )
+    host = f"{config('ES_HOST')}/{config('ES_DATABASE')}"
+    basic_auth = (config("ES_USERNAME"), config("ES_PASSWORD"))
+    logging.info("Connecting to Elasticsearch, {} {}", host, basic_auth)
+    client = Elasticsearch(host, basic_auth=basic_auth)
     logging.info("Connection to Elasticsearch successful")
-except Exception:
-    logging.error("Connection to Elasticsearch failed")
+except Exception as e:
+    logging.error("Connection to Elasticsearch failed, {}", e)
 
 
 def main():
     logging.info("Checking connection to Elasticsearch")
+    ping = client.ping()
+    logging.info("Ping to Elasticsearch successful, {}", ping)
     return client.info()
