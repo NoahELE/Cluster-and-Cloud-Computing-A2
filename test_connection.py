d550747ad578d9b73a6b12d781e477555cd6732a
NoahELE <cxhmel@outlook.com>
2024-05-04 22:23:52 +1000

    fix config key error

--- test_connection.py
+++ test_connection.py
@@ -5,7 +5,7 @@ from elasticsearch import Elasticsearch
 from k8s_utils import config
 
 try:
-    host = f"{config('ES_HOST')}/{config('ES_DATABASE')}"
+    host = f"{config('ES_URL')}/{config('ES_DATABASE')}"
     basic_auth = (config("ES_USERNAME"), config("ES_PASSWORD"))
     logging.info("Connecting to Elasticsearch, {} {}", host, basic_auth)
     client = Elasticsearch(host, basic_auth=basic_auth)
