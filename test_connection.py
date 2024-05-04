b2807b563533e9e9f20fa86220b76850c93c31da
NoahELE <cxhmel@outlook.com>
2024-05-04 22:41:05 +1000

    remove unnecessary logging

--- test_connection.py
+++ test_connection.py
@@ -4,14 +4,9 @@ from elasticsearch import Elasticsearch
 
 from k8s_utils import config
 
-try:
-    host = f"{config('ES_URL')}/{config('ES_DATABASE')}"
-    basic_auth = (config("ES_USERNAME"), config("ES_PASSWORD"))
-    logging.info(f"Connecting to Elasticsearch, {host} {basic_auth}")
-    client = Elasticsearch(host, basic_auth=basic_auth)
-    logging.info("Connection to Elasticsearch successful")
-except Exception as e:
-    logging.error(f"Connection to Elasticsearch failed, {e}")
+host = f"{config('ES_URL')}/{config('ES_DATABASE')}"
+basic_auth = (config("ES_USERNAME"), config("ES_PASSWORD"))
+client = Elasticsearch(host, basic_auth=basic_auth)
 
 
 def main():
