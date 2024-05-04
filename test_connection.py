7254a152035ec9e06abafd9979b72defbebc4265
NoahELE <cxhmel@outlook.com>
2024-05-04 22:28:16 +1000

    fix logging format

--- test_connection.py
+++ test_connection.py
@@ -7,15 +7,15 @@ from k8s_utils import config
 try:
     host = f"{config('ES_URL')}/{config('ES_DATABASE')}"
     basic_auth = (config("ES_USERNAME"), config("ES_PASSWORD"))
-    logging.info("Connecting to Elasticsearch, {} {}", host, basic_auth)
+    logging.info(f"Connecting to Elasticsearch, {host} {basic_auth}")
     client = Elasticsearch(host, basic_auth=basic_auth)
     logging.info("Connection to Elasticsearch successful")
 except Exception as e:
-    logging.error("Connection to Elasticsearch failed, {}", e)
+    logging.error(f"Connection to Elasticsearch failed, {e}")
 
 
 def main():
     logging.info("Checking connection to Elasticsearch")
     ping = client.ping()
-    logging.info("Ping to Elasticsearch successful, {}", ping)
+    logging.info(f"Ping to Elasticsearch successful, {ping}")
     return client.info()
