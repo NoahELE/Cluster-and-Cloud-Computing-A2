ab58167f4dbd14de329bb39c664cd9a43d81e6e6
NoahELE <cxhmel@outlook.com>
2024-05-04 19:26:57 +1000

    add logging statements to test connection

--- test_connection.py
+++ test_connection.py
@@ -1,12 +1,19 @@
+import logging
+
 from elasticsearch import Elasticsearch
 
 from k8s_utils import config
 
-client = Elasticsearch(
-    f"{config("ES_HOST")}/{config("ES_DATABASE")}",
-    basic_auth=(config("ES_USERNAME"), config("ES_PASSWORD")),
-)
+try:
+    client = Elasticsearch(
+        f"{config("ES_HOST")}/{config("ES_DATABASE")}",
+        basic_auth=(config("ES_USERNAME"), config("ES_PASSWORD")),
+    )
+    logging.info("Connection to Elasticsearch successful")
+except Exception:
+    logging.error("Connection to Elasticsearch failed")
 
 
 def main():
+    logging.info("Checking connection to Elasticsearch")
     return client.info()
