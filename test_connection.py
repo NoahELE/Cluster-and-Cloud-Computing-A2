384b34c2b2914976596e24a283a59eadf855ed9b
NoahELE <cxhmel@outlook.com>
2024-05-08 16:16:14 +1000

    do not verify certs

--- test_connection.py
+++ test_connection.py
@@ -7,6 +7,6 @@ def main():
     host = "https://elasticsearch-master.elastic.svc.cluster.local:9200"
     # basic_auth = (config("ES_USERNAME"), config("ES_PASSWORD"))
     basic_auth = ("elastic", "elastic")
-    client = Elasticsearch(host, basic_auth=basic_auth)
+    client = Elasticsearch(host, basic_auth=basic_auth, verify_certs=False)
     current_app.logger.info("Checking connection to Elasticsearch")
     return jsonify(client.info())
