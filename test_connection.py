e80e9503e4c22f5e3b0f21b56e501e6498f8786e
NoahELE <cxhmel@outlook.com>
2024-05-08 16:01:47 +1000

    return json from main

--- test_connection.py
+++ test_connection.py
@@ -1,13 +1,13 @@
 from elasticsearch import Elasticsearch
-from flask import current_app
+from flask import current_app, jsonify
 
 from k8s_utils import config
 
-host = f"{config('ES_URL')}/{config('ES_DATABASE')}"
+host = f"{config('ES_URL')}"
 basic_auth = (config("ES_USERNAME"), config("ES_PASSWORD"))
 client = Elasticsearch(host, basic_auth=basic_auth)
 
 
 def main():
     current_app.logger.info("Checking connection to Elasticsearch")
-    return client.info()
+    return jsonify(client.info())
