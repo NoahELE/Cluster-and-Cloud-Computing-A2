8f4965e5d4c9e8ffb56f5cd5f59f911af676343c
KejingL1 <kejingl1@student.unimelb.edu.au>
2024-05-18 18:01:47 +1000

    improve security for getting weather data

--- insert_bom_weather_past_data.py
+++ insert_bom_weather_past_data.py
@@ -1,7 +1,10 @@
 import json
 import os
+from dotenv import load_dotenv
 from elasticsearch import Elasticsearch, helpers
 from datetime import datetime
+
+load_dotenv()
 
 def format_date(date_str):
     date_obj = datetime.strptime(date_str, '%Y-%m-%d')
@@ -11,9 +14,9 @@ def format_date(date_str):
 def insert_past_weather(file_path, index_name):
     # Establish connection to Elasticsearch
     es = Elasticsearch(
-        'https://127.0.0.1:9200',
-        basic_auth=('elastic', 'gHcmDFVtcTaCkB4QPVHSYkEe7bTbYd!x'),
-        verify_certs=False
+    "https://localhost:9200",
+    basic_auth=(os.environ["ES_USERNAME"], os.environ["ES_PASSWORD"]),
+    verify_certs=False,
     )
     
     with open(file_path, 'r', encoding='utf-8') as file:
--- test/test_traffic_rainfall.py
+++ test/test_traffic_rainfall.py
@@ -3,15 +3,18 @@ from datetime import datetime
 
 
 def test_traffic_count():
-    resp = requests.get("http://localhost:9090/traffic-accident/accident-count/2023-04-01")
+    resp = requests.get(
+        "http://localhost:9090/traffic-accident/accident-count/2023-04-01"
+    )
     resp_json = resp.json()
     assert resp_json["ok"]
     assert isinstance(resp_json["accident_count"], int)
     assert isinstance(resp_json["center_accidents"], int)
+
 
 def test_rainfall():
     resp = requests.get("http://localhost:9090/bom-past-rainfall/rainfall/2023-04-01")
     resp_json = resp.json()
     assert resp_json["ok"]
     assert isinstance(resp_json["rainfall_amount"], float)
-    assert isinstance(resp_json["rainfall_date"], datetime)
+    assert isinstance(resp_json["rainfall_date"], str)
