2623c1fb306e6ec3e8d0f063e3584c1900ccec4d
NoahELE <cxhmel@outlook.com>
2024-05-18 17:19:13 +1000

    reorganize code structure

--- test_mastodon.py
+++
@@ -1,8 +0,0 @@
-from mastodon import Mastodon
-
-mastodon = Mastodon(
-    api_base_url="https://aus.social",
-)
-
-toots = mastodon.timeline_hashtag("melbourne")
-print([toot["id"] for toot in toots])
