e18ec149821e81cfac147c7a551566bacea2d63d
KejingL1 <kejingl1@student.unimelb.edu.au>
2024-05-21 17:54:46 +1000

    modify type error

--- test/test_air_quality.py
+++ test/test_air_quality.py
@@ -1,8 +1,10 @@
 import requests
 
 
-def test_health():
-    resp = requests.get("http://localhost:9090/air-quality/air-quality-avg/Box")
+def test_air_quality():
+    resp = requests.get(
+        "http://localhost:9090/air-quality/air-quality-avg/Box/2021-01-01/365"
+    )
     resp_json = resp.json()
     assert resp_json["ok"]
     assert isinstance(resp_json["avg_BPM25"], float)
